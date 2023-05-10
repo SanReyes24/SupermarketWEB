5e9874d0b4b78a9c0de6ca0160ae8ad7cf2fd3d9
Sandra Liliana Reyes Castiblanco <sreyesc24@gmail.com>
2023-05-09 19:38:01 -0500

    Index Customers

---
+++ Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace SupermarketWEB.Models
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Identification { get; set; }
+		public string Observation { get; set; }
+	}
+}
