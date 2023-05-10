c1ba423684a815fc003926daf9fcffa3981b271c
Sandra Liliana Reyes Castiblanco <sreyesc24@gmail.com>
2023-05-09 20:55:16 -0500

    Index Sales

---
+++ Pages/Sales/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Sales
+{
+	public class IndexModel : PageModel
+	{
+		private readonly SupermarketContext _context;
+
+		public IndexModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IList<Sell?> Sales { get; set; } = default!;
+
+		public async Task OnGetAsync()
+		{
+			if (_context.Sales != null)
+			{
+				Sales = await _context.Sales.ToListAsync();
+			}
+
+		}
+	}
+}
